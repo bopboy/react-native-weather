e346a7d46619f074c5bb521fdf9fc59af815bf7b
bopboy <39391891+bopboy@users.noreply.github.com>
2022-01-25 23:54:46 +0900

    15 위치 정보 (expo-location 이용)

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
+import { useEffect, useState } from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native'
+import * as Location from 'expo-location'
+
 const { width: SCREEN_WIDTH } = Dimensions.get("window")
 
 export default function App() {
+  const [city, setCity] = useState("Loading...")
+  const [ok, setOk] = useState(true)
+  const ask = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync()
+    if (!granted) setOk(false)
+    const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync({ accuracy: 5 })
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude }, { useGoogleMaps: false }
+    )
+    setCity(location[0].city)
+  }
+  useEffect(() => {
+    ask()
+  }, [])
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView horizontal pagingEnabled
         showsHorizontalScrollIndicator={false}
